6e2b438a031d90f0b7c59f1fe6e17a148822c813
Emmanuel Nnadi <ennadi@my.dom.edu>
2023-09-25 15:28:48 -0500

    React/JavaScript/TypeScript and Html example codes

---
+++ Example Hooks - useCallback.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState, useCallback } from 'react';
+
+export default function Form() {
+    const [value, setValue] = useState("Change me");
+
+    const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>((event) => {
+        setValue(event.currentTarget.value);
+    }, [setValue])
+
+    return (
+        <>
+            <input value={value} onChange={handleChange} />
+            <p>Value: {value}</p>
+        </>
+    );
+}
