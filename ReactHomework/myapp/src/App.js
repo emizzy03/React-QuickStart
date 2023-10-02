0a34d69047fc9aa5290ebc543a603a0a54f6f4f9
Emmanuel Nnadi <ennadi@my.dom.edu>
2023-10-02 16:54:42 -0500

    React App
    First React application creating a simple  tic-tac-toe game.

---
+++ ReactHomework/myapp/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+
+const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(''));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)} style={{ width: '70px', height: '70px' }}>
+        {board[index]}
+
+      </button>
+    );
+  };
+
+  const calculateWinner = (squares) => {
+    const winningLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winningLines.length; i++) {
+      const [a, b, c] = winningLines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const winner = calculateWinner(board);
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (board.every((square) => square !== '')) {
+    status = 'It\'s a draw!';
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <div className="board-row"> <style>squares</style>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TicTacToe;
